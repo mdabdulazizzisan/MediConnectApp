558fe24ffe20ec723e8195dc8cf9caefdeec1fd5
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-04-12 10:32:48 +0600

    initial commit

---
+++ app/src/main/java/com/kolu/mediconnect/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.kolu.mediconnect
+
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.kolu.mediconnect.navigation.MainNavGraph
+import com.kolu.mediconnect.ui.theme.MediConnectTheme
+
+class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MediConnectTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainNavGraph(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
